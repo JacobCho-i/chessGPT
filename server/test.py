f38de20aa8458cc512016672056de31fbf86a0c7
Jacob Choi <junhyungch@gmail.com>
2023-12-24 02:20:11 -0500

    basic front-end back-end connection

--- server/test.py
+++ server/test.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, request, jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -7,6 +7,20 @@ CORS(app)
 @app.route('/api/data')
 def get_data():
     return jsonify({'data': 'Someone ban malzhar!'})
+
+@app.route('/process_data', methods=['POST'])
+def process_data():
+    data = request.json  # Get JSON data sent by the client
+    # Process data here...
+    print(data)
+    return jsonify({'data': 'Data received successfully!'})
+
+@app.route('/process_champion', methods=['POST'])
+def process_champion():
+    data = request.json  # Get JSON data sent by the client
+    # Process data here...
+    print(data)
+    return jsonify({'data': 'Data received successfully!'})
 
 if __name__ == '__main__':
     app.run(debug=True)
--- src/App.js
+++ src/App.js
@@ -1,22 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
-import React, { useEffect, useState } from 'react';
+  import logo from './logo.svg';
+  import './App.css';
+  import React, { useEffect, useState } from 'react';
 
-function App() {
+  function App() {
 
-  const [data, setData] = useState('');
+    const [data, setData] = useState('');
+    const [status, setStatus] = useState('');
+    const [champ, setChamp] = useState('');
+    const [champdata, setChampdata] = useState('');
 
-  useEffect(() => {
-    fetch('http://localhost:5000/api/data')
+    useEffect(() => {
+      fetch('http://localhost:5000/api/data')
+        .then(response => response.json())
+        .then(data => setData(data.data));
+      const dataToSend = { key1: 'value1', key2: 'value2' };
+      sendDataToPython(dataToSend);
+    }, []);
+
+    function sendDataToPython(data) {
+      fetch('http://localhost:5000/process_data', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+      })
       .then(response => response.json())
-      .then(data => setData(data.data));
-  }, []);
+      .then(data => setStatus(data.data))
+      .catch(error => console.error('Error:', error));
+    }
 
-  return (
-    <div>
-      <p>Data from Python: {data}</p>
-    </div>
-  );
-}
+    function selectChamp(name) {
+      setChamp(name);
+      const dataToSend = {champion: name};
+      fetch('http://localhost:5000/process_champion', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(dataToSend),
+      })
+      .then(response => response.json())
+      .then(data => console.log(data))
+      .catch(error => console.error('Error:', error));
+    }
 
-export default App;
+    return (
+      <div>
+        <p>Data from Python: {data}</p>
+        <p>input received in Python: {status}</p>
+        <p>Pick your champion. Your choice : {champ}</p>
+        <button onClick={() => selectChamp("Malzhar")}>Malzhar</button>
+        <button onClick={() => selectChamp("Garen")}>Garen</button>
+        <button onClick={() => selectChamp("Sylas")}>Sylas</button>
+      </div>
+    );
+  }
+
+  export default App;
