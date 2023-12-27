197e13f260cf9c6e6a9c8d0f1d65ccda547a9d75
Jacob Choi <junhyungch@gmail.com>
2023-12-27 13:11:21 -0500

    setting up message mapping for responses

--- server/test.py
+++ server/test.py
@@ -3,6 +3,7 @@ from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
+index = 1
 
 @app.route('/api/data')
 def get_data():
@@ -20,7 +21,18 @@ def process_champion():
     data = request.json  # Get JSON data sent by the client
     # Process data here...
     print(data)
-    return jsonify({'data': 'Data received successfully!'})
+    msg = 'My first move is ' + data['champion'] + '!'
+    return jsonify({'data': msg})
+
+# url = '/message' + str(index)
+@app.route('/message', methods=['POST'])
+def process_msg():
+    data = request.json  # Get JSON data sent by the client
+    # Process data here...
+    print(data)
+    msg = 'My first move is ' + data['champion'] + '!'
+    # index = index + 1
+    return jsonify({'data': msg})
 
 if __name__ == '__main__':
     app.run(debug=True)
--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-  import logo from './logo.svg';
   import './App.css';
   import React, { useEffect, useState } from 'react';
 
-  function App() {
+  console.log("hello");
+  let messages = [];
 
+  function App() {
+    
     const [data, setData] = useState('');
     const [status, setStatus] = useState('');
     const [champ, setChamp] = useState('');
-    const [champdata, setChampdata] = useState('');
+    const [msg, setMsg] = useState('');
 
     useEffect(() => {
       fetch('http://localhost:5000/api/data')
@@ -29,11 +31,14 @@
       .then(data => setStatus(data.data))
       .catch(error => console.error('Error:', error));
     }
+    
 
-    function selectChamp(name) {
+    function processMessage(name) {
+      console.log(messages)
       setChamp(name);
       const dataToSend = {champion: name};
-      fetch('http://localhost:5000/process_champion', {
+      //let url = "http://localhost:5000/message" + index;
+      fetch("http://localhost:5000/message", {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
@@ -41,18 +46,39 @@
         body: JSON.stringify(dataToSend),
       })
       .then(response => response.json())
-      .then(data => console.log(data))
+      .then(data => compareAndAppend(data.data))
       .catch(error => console.error('Error:', error));
+      // setIndex(index + 1);
+    }
+
+    function compareAndAppend(message) {
+      if (msg !== message) {
+        messages.push(message);
+        console.log(messages)
+        setMsg(message)
+      }
     }
 
     return (
-      <div>
-        <p>Data from Python: {data}</p>
-        <p>input received in Python: {status}</p>
-        <p>Pick your champion. Your choice : {champ}</p>
-        <button onClick={() => selectChamp("Malzhar")}>Malzhar</button>
-        <button onClick={() => selectChamp("Garen")}>Garen</button>
-        <button onClick={() => selectChamp("Sylas")}>Sylas</button>
+      <div className="flex justify-between items-start h-screen">
+        <div>
+          <p>Data from Python: {data}</p>
+          <p>input received in Python: {status}</p>
+          <p>Pick your champion. Your choice : {champ}</p>
+          <div className='py-4 px-4'>
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4" onClick={() => processMessage("Malzhar")}>Malzhar</button>
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 mx-4" onClick={() => processMessage("Garen")}>Garen</button>
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4" onClick={() => processMessage("Sylas")}>Sylas</button>
+          </div>
+        </div>
+        <div className='mx-16 max-h-[300px] overflow-y-auto w-[300px]'>
+          <p>Message from ChatGPT: </p>
+          <ul>
+            {messages.map((message, index) => (
+              <li key={index}>{message}</li>
+            ))}
+          </ul>
+        </div>
       </div>
     );
   }
